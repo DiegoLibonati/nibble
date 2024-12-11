39b8cfbb7de7dd77c18b70aa1ed807e29e9f4f6e
Die <dlibonati@leafnoise.io>
2024-12-11 11:39:17 -0300

    patch: tests updated

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jest-fixed-jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/tests/jest.setup.ts"],
-  transformIgnorePatterns: [
-    "node_modules/(?!@bundled-es-modules)/",
-    "node_modules/(?!variables/.*)",
-  ],
   moduleNameMapper: {
     "\\.(css|scss)$": "<rootDir>/src/tests/mocks/files/styleMock.js",
   },
